2f29850156ea1f223b99c3cdfa9c8b4b1789059b
tonuimartin <112543423+tonuimartin@users.noreply.github.com>
2023-10-02 11:18:02 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Test</title>
+    </head>
+    <body>
+        <?php echo '<p>This is ICSB serveradmin group 10</p>'; ?>
+    </body>
+</html>
